48ca14b10a9dc8525ddaf032eb6c5196ec96a43d
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-09-09 15:07:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+#--------------------------------------------#
+#   该部分代码只用于看网络结构，并非测试代码
+#   map测试请看get_dr_txt.py、get_gt_txt.py
+#   和get_map.py
+#--------------------------------------------#
 import torch
 from torchsummary import summary
 from nets.yolo3 import YoloBody
