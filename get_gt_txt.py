bca67afa0e5c1723faa67aeac7c80e9e10753cf9
Bubbliiiing <47347516+bubbliiiing@users.noreply.github.com>
2020-11-02 14:05:51 +0800

    Add files via upload

--- get_gt_txt.py
+++ get_gt_txt.py
@@ -7,7 +7,6 @@ import sys
 import os
 import glob
 import xml.etree.ElementTree as ET
-from tqdm import tqdm
 
 image_ids = open('VOCdevkit/VOC2007/ImageSets/Main/test.txt').read().strip().split()
 
@@ -16,19 +15,24 @@ if not os.path.exists("./input"):
 if not os.path.exists("./input/ground-truth"):
     os.makedirs("./input/ground-truth")
 
-for image_id in tqdm(image_ids):
+for image_id in image_ids:
     with open("./input/ground-truth/"+image_id+".txt", "w") as new_f:
         root = ET.parse("VOCdevkit/VOC2007/Annotations/"+image_id+".xml").getroot()
         for obj in root.findall('object'):
+            difficult_flag = False
             if obj.find('difficult')!=None:
                 difficult = obj.find('difficult').text
                 if int(difficult)==1:
-                    continue
+                    difficult_flag = True
             obj_name = obj.find('name').text
             bndbox = obj.find('bndbox')
             left = bndbox.find('xmin').text
             top = bndbox.find('ymin').text
             right = bndbox.find('xmax').text
             bottom = bndbox.find('ymax').text
-            new_f.write("%s %s %s %s %s\n" % (obj_name, left, top, right, bottom))
+            if difficult_flag:
+                new_f.write("%s %s %s %s %s difficult\n" % (obj_name, left, top, right, bottom))
+            else:
+                new_f.write("%s %s %s %s %s\n" % (obj_name, left, top, right, bottom))
+
 print("Conversion completed!")
