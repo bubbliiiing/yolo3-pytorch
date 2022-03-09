1fff15a8a5ea7622a6df73b4b3e8e1a610e72fc5
Bubbliiiing <3323290568@qq.com>
2022-03-09 21:09:22 +0800

    update comment

--- utils/callbacks.py
+++ utils/callbacks.py
@@ -18,8 +18,11 @@ class LossHistory():
         
         os.makedirs(self.log_dir)
         self.writer     = SummaryWriter(self.log_dir)
-        dummy_input     = torch.randn(2, 3, input_shape[0], input_shape[1])
-        self.writer.add_graph(model, dummy_input)
+        try:
+            dummy_input     = torch.randn(2, 3, input_shape[0], input_shape[1])
+            self.writer.add_graph(model, dummy_input)
+        except:
+            pass
 
     def append_loss(self, epoch, loss, val_loss):
         if not os.path.exists(self.log_dir):
